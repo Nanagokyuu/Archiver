73c7c1764cd9ad0d07863c7e2f0ec945003fdd2b
Nanagokyuu <wangyifengsw@outlook.com>
2026-04-13 15:18:57 +0800

    Initial Commit

---
+++ Unzip/UnzipApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnzipApp.swift
+//  Unzip
+//
+//  Created by Nanagokyuu on 2026/4/13.
+//
+
+import SwiftUI
+
+@main
+struct UnzipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
